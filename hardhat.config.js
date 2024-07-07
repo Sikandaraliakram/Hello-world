87c3ff29a331feae99b03d90fa856137201e8bc7
Sikandar Ali Akram <sikandaraliakram1@gmail.com>
2024-07-07 23:18:42 +0500

    First Hello World Web3 App

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,18 @@
+require("dotenv").config();
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-ethers");
+
+const { PRIVATE_KEY } = process.env;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
+  defaultNetwork: "sepolia",
+  networks: {
+    hardhat: {},
+    sepolia: {
+      url: "https://eth-sepolia.g.alchemy.com/v2/NTZ4RfC-Utsv3M7WX10y4wUVq0nNRSSD",
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+  },
 };
